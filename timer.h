1521e02319c16d94ede6a69287abb598182bc712
Dean Knight <dakknight@gmail.com>
2013-01-28 19:38:28 -0500

    Added neccessary function prototypes

--- timer.h
+++ timer.h
@@ -2,4 +2,7 @@
 #define TIMER_H
 void timer_init(void);
 unsigned int uptime(void);
+void uptime_to_string(unsigned int uptime, char* string);
+void print_time_diff(unsigned int device, unsigned int current, unsigned int last);
+
 #endif
