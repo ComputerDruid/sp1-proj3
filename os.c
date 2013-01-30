698df6f5eac97835c680f78db22191150b40eb39
Dan Johnson <ComputerDruid@gmail.com>
2013-01-30 15:37:07 -0500

    os: ignore USB device interrupts in vector 0x2A
    I want to be able to use the program without the USB device plugged in,
    so that we can load the next image without stopping the test.

--- os.c
+++ os.c
@@ -1,10 +1,19 @@
 #include "sio.h"
 #include "timer.h"
+#include "x86arch.h"
+#include "support.h"
+#include "startup.h"
+static void usb_interrupt_handler( int vector, int code ){
+	__outb( PIC_MASTER_CMD_PORT, PIC_EOI );
+	__outb( PIC_SLAVE_CMD_PORT, PIC_EOI );
+}
 void os_init(void) {
 	/*
 	** Initialize the serial port
 	*/
 	sio_init();
 	timer_init();
+	//Ignore the USB device interrupt
+	__install_isr( 0x2a, usb_interrupt_handler);
 	__asm("sti");
 }
