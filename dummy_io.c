68b54366ad58c737a35cc03b5fd61cca93ee1732
Dan Johnson <ComputerDruid@gmail.com>
2013-02-02 16:27:47 -0500

    dummy_io: add documentation comments

--- dummy_io.c
+++ dummy_io.c
@@ -1,3 +1,14 @@
+/**
+ * Dummy IO module:
+ * Does nothing for all the specified IO commnds
+ *
+ * Obviously only useful for debugging.
+ * Author: Dan Johnson
+ */
+
+/**
+ * Always gets an empty string
+ */
 int dummy_gets(char *buffer, unsigned int count) {
 	if (count) {
 		*buffer = '\0';
@@ -9,8 +20,14 @@ int dummy_gets(char *buffer, unsigned int count) {
 		return -1;
 	}
 }
+/**
+ * Never gets a character
+ */
 int dummy_getchar(void) {
 	return 0;
 }
+/**
+ * Ignores the given string
+ */
 void dummy_puts(char *str) {
 }
--- dummy_io.h
+++ dummy_io.h
@@ -1,3 +1,7 @@
+/**
+ * External interface for the dummy_io module. Used to add methods to table in
+ * io module.
+ */
 #ifndef DUMMY_IO_H
 #define DUMMY_IO_H
 int dummy_gets(char *str, unsigned int count);
