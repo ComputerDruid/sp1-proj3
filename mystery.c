fb7a3d4677eacfe5f265de7828e6a7fbe972a081
Dan Johnson <ComputerDruid@gmail.com>
2013-02-02 16:29:37 -0500

    mystery: add documentation comments

--- mystery.c
+++ mystery.c
@@ -1,9 +1,26 @@
+/**
+ * mystery menu written just for fun.
+ * Author: Dan Johnson
+ */
+
 #include "io.h"
 #include "support.h"
+
+/**
+ * Triple fault and reset the machine
+ */
 static void tfault(void) {
+	//load invalid IDT
 	__asm("lidt 0");
+	//cause an interrupt
 	__asm("int3");
 }
+
+/**
+ * Secret menu only accessible via special character combination.
+ * Allows the user to reset the machine or leave the menu.
+ * @param d the device to convert into a menu.
+ */
 void mystery(device_t d) {
 	dputs(d, "Hah!\n");
 	dputs(d, "s - System RESET : r - return to normal mode\n");
--- mystery.h
+++ mystery.h
@@ -1,3 +1,8 @@
+/**
+ * External interface for the mystery menu. To be used in the watch menu
+ *
+ * Author: Dan Johnson
+ */
 #ifndef MYSTERY_H
 #define MYSTERY_H
 void mystery(device_t d);
